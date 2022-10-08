8394735b797ff10e9c01d61ba5b4a0fc1c395e6f
walterpaulo <walter0paulo@hotmail.com>
2022-10-08 06:40:39 -0300

    feat: added class 6

--- aulas/dia4/ecommerce_console/src/main/java/br/com/unoinfo/App.java
+++ aulas/dia4/ecommerce_console/src/main/java/br/com/unoinfo/App.java
@@ -16,6 +16,81 @@ public class App
 	    	
     	/*
     	 * Autor: Walter Paulo
+    	 * Date: 08/10/2022
+    	 * Exercise: 1 do dia 6 - Desafio 21 dias em Java 
+    	 */
+	    	
+	    /*
+	     * Paula tem um petshop, e está precisando de um sistema
+	     * para controlar seus serviços, no sistema, terá cadastro
+	     * de cliente (dados Básicos) os dados o peso do Pet,
+	     * depois o sistema deverá calcular o valor do banho pelo o peso do Pet,
+	     * segue este cáculo:
+	     * 
+	     * Para macho:
+	     * (pesoDoPeto * valorPorPeso) + (pesoDoPet * valorPorPeso) * 15 / 100
+	     * 
+	     * Para fêmea:
+	     * (pesoDoPeto * valorPorPeso) + (pesoDoPet * valorPorPeso) * 30 / 100
+	     * 
+	     * Feito o caculo o sistema deverá mostrar um relatório com os dados do pedido
+	     * e o valor total a pagar.
+	     * 
+	     * Coloque uma usuabilidade agradável.
+	     */
+	    	
+	    	
+	    System.out.println("===============[ PetAni ]===============");
+	    System.out.println("Olá Paula, bom dia");
+	    System.out.println("===============[ Cadastro Cliente ]===============");
+	    System.err.println("Digite o nome");
+	    var nomeDoCliente = ler.next();
+	    System.out.println("Digite o telefone");
+	    var telefoneCliente = ler.next();
+	    System.out.println("===============[ Cadastro do Pet ]===============");
+	    System.err.println("Digite o nome");
+	    var nomeDoPet = ler.next();
+	    System.err.println("Digite o sexo: M => macho / F => Fêmea");
+	    var sexoDoPet = ler.next();
+	    System.err.println("Digite o peso:");
+	    var pesoDoPet = ler.nextDouble();
+	    
+	    System.out.println("===============[ Cadastro do Preço ]===============");
+	    System.err.println("Digite o valor por peso");
+	    var valorPorPeso = ler.nextDouble();
+	    var valorTotal = 0.0;
+	    if(sexoDoPet.equals("M")) {
+	    	valorTotal =  (valorPorPeso * valorPorPeso) + (pesoDoPet * valorPorPeso) * 15 / 100;
+	    } else {
+	    	valorTotal =  (valorPorPeso * valorPorPeso) + (pesoDoPet * valorPorPeso) * 30 / 100;
+	    }
+	    DecimalFormat moedaBr = new DecimalFormat( "#,###,###,##0.00" );
+	    
+	    System.out.println("============[ Relatório ]=======================");
+	    System.out.println("Nome do cliente: "+ nomeDoCliente);
+	    System.out.println("Telefone: "+ telefoneCliente);
+	    System.out.println("\n-----[ Pet ]--------");
+	    System.out.println("Nome do Pet: "+nomeDoCliente);
+	    System.out.println("Sexo: "+sexoDoPet);
+	    System.out.println("Peso: "+pesoDoPet);
+	    System.out.println("\n---------[ Pagamento ]--------");
+	    System.err.println("Valor Total R$ "+ moedaBr.format(valorTotal));
+	    
+	    
+	    
+//	    (pesoDoPeto * valorPorPeso) + (pesoDoPet * valorPorPeso) * 15 / 100
+	    
+//	    Para macho:
+//		     * 
+//		     * 
+//		     * Para fêmea:
+//		     * (pesoDoPeto * valorPorPeso) + (pesoDoPet * valorPorPeso) * 30 / 100	
+	    	
+	    	
+	    	
+	    	
+    	/*
+    	 * Autor: Walter Paulo
     	 * Date: 06/10/2022
     	 * Exercise: 1 do dia 5 - Desafio 21 dias em Java 
     	 */
@@ -43,36 +118,36 @@ public class App
     	 * 
     	 */
     	
-    	System.out.println("=======[ Calculo do Material de Construção ]=============");
-    	System.out.println("Olá João seja muito bem vindo, vamos começar?");
-    	System.out.println("Digite o nome do seu Cliente:");
-    	var nome = ler.next();
-    	
-    	System.out.print("Digite o endereço:");
-    	var enderecao = ler.next();
-    	
-    	System.out.print("Digite nome do material: ");
-    	var material = ler.next();
-    	
-    	System.err.printf("Digite o valor unitário de %s: ", material);
-    	var valorMaterial = ler.nextFloat();
-    	
-    	System.out.printf("Digite a quantidade de %s: ", material);
-    	var quantidadeMaterial = ler.nextInt();
-    	
-    	var valorTotal = valorMaterial * quantidadeMaterial;
-
-    	DecimalFormat moedaBr = new DecimalFormat( "#,###,###,##0.00" );
-    	
-    	System.out.println("====================================");
-    	System.out.println("Cliente: "+ nome);
-    	System.out.println("Endereco de entrega: "+ enderecao);
-    	System.out.printf("Comprou o material: %s no valor de R$ %s", material, moedaBr.format(valorMaterial));
-    	System.out.println("\nO valor total da compra deu: R$ "+ moedaBr.format(valorMaterial));
-    	if(valorTotal > 100)
-    		System.out.println("\nObs: Cliente terá desconto na próxima.");
-    	System.out.println("====================================");
-    	
+//    	System.out.println("=======[ Calculo do Material de Construção ]=============");
+//    	System.out.println("Olá João seja muito bem vindo, vamos começar?");
+//    	System.out.println("Digite o nome do seu Cliente:");
+//    	var nome = ler.next();
+//    	
+//    	System.out.print("Digite o endereço:");
+//    	var enderecao = ler.next();
+//    	
+//    	System.out.print("Digite nome do material: ");
+//    	var material = ler.next();
+//    	
+//    	System.err.printf("Digite o valor unitário de %s: ", material);
+//    	var valorMaterial = ler.nextFloat();
+//    	
+//    	System.out.printf("Digite a quantidade de %s: ", material);
+//    	var quantidadeMaterial = ler.nextInt();
+//    	
+//    	var valorTotal = valorMaterial * quantidadeMaterial;
+//
+//    	DecimalFormat moedaBr = new DecimalFormat( "#,###,###,##0.00" );
+//    	
+//    	System.out.println("====================================");
+//    	System.out.println("Cliente: "+ nome);
+//    	System.out.println("Endereco de entrega: "+ enderecao);
+//    	System.out.printf("Comprou o material: %s no valor de R$ %s", material, moedaBr.format(valorMaterial));
+//    	System.out.println("\nO valor total da compra deu: R$ "+ moedaBr.format(valorMaterial));
+//    	if(valorTotal > 100)
+//    		System.out.println("\nObs: Cliente terá desconto na próxima.");
+//    	System.out.println("====================================");
+//    	
     	
     	
     	
---
+++ aulas/dia6/README.md
@@ -0,0 +1,99 @@
+## Aula 6 - 08/10/2022 - [Desafio 21 dias em Java](../../README.md)
+
+### Exercícios
+
+* Exercício 2
+
+```s
+ O governo do Brasil está precisando construir uma ponte
+ que possibilita a passage de caminhoẽos de um estado a outro,
+ a ponte ajudará no transporte de alimentos, e alimentos diversos de brasileiros.
+
+ Faça um programa que ajude a calcular o valor total da ponte.
+ Para isso você precisa pergutar alguns itens:
+
+ - valor pro metro quadrado;
+ - total de mentro da ponte;
+ - mostrar as opçoes de ferro da fundação da ponte, assim o engenheiro pode escolher
+ uma opção, exemplo: ( ferro fundido, Ferro com aço, ferro normal);
+ - o valor do ferro varia de acordo com a opção escolhido;
+ - no final do relatório mostre o valor total construido;e
+ - desse valor total contruído, mostrar também que 30% foi adquirido
+ com o valor de imposto arracadado sobre a população local;
+
+ Objeto do exercício:
+ - utilizar if;e
+ - switch
+
+```
+
+
+* Exercicio 1, feito em aula
+
+```java
+Scanner ler = new Scanner(System.in);
+	    	
+    	/*
+    	 * Autor: Walter Paulo
+    	 * Date: 08/10/2022
+    	 * Exercise: 1 do dia 6 - Desafio 21 dias em Java 
+    	 */
+	    	
+	    /*
+	     * Paula tem um petshop, e está precisando de um sistema
+	     * para controlar seus serviços, no sistema, terá cadastro
+	     * de cliente (dados Básicos) os dados o peso do Pet,
+	     * depois o sistema deverá calcular o valor do banho pelo o peso do Pet,
+	     * segue este cáculo:
+	     * 
+	     * Para macho:
+	     * (pesoDoPeto * valorPorPeso) + (pesoDoPet * valorPorPeso) * 15 / 100
+	     * 
+	     * Para fêmea:
+	     * (pesoDoPeto * valorPorPeso) + (pesoDoPet * valorPorPeso) * 30 / 100
+	     * 
+	     * Feito o caculo o sistema deverá mostrar um relatório com os dados do pedido
+	     * e o valor total a pagar.
+	     * 
+	     * Coloque uma usuabilidade agradável.
+	     */
+	    	
+	    	
+	    System.out.println("===============[ PetAni ]===============");
+	    System.out.println("Olá Paula, bom dia");
+	    System.out.println("===============[ Cadastro Cliente ]===============");
+	    System.err.println("Digite o nome");
+	    var nomeDoCliente = ler.next();
+	    System.out.println("Digite o telefone");
+	    var telefoneCliente = ler.next();
+	    System.out.println("===============[ Cadastro do Pet ]===============");
+	    System.err.println("Digite o nome");
+	    var nomeDoPet = ler.next();
+	    System.err.println("Digite o sexo: M => macho / F => Fêmea");
+	    var sexoDoPet = ler.next();
+	    System.err.println("Digite o peso:");
+	    var pesoDoPet = ler.nextDouble();
+	    
+	    System.out.println("===============[ Cadastro do Preço ]===============");
+	    System.err.println("Digite o valor por peso");
+	    var valorPorPeso = ler.nextDouble();
+	    var valorTotal = 0.0;
+	    if(sexoDoPet.equals("M")) {
+	    	valorTotal =  (valorPorPeso * valorPorPeso) + (pesoDoPet * valorPorPeso) * 15 / 100;
+	    } else {
+	    	valorTotal =  (valorPorPeso * valorPorPeso) + (pesoDoPet * valorPorPeso) * 30 / 100;
+	    }
+	    DecimalFormat moedaBr = new DecimalFormat( "#,###,###,##0.00" );
+	    
+	    System.out.println("============[ Relatório ]=======================");
+	    System.out.println("Nome do cliente: "+ nomeDoCliente);
+	    System.out.println("Telefone: "+ telefoneCliente);
+	    System.out.println("\n-----[ Pet ]--------");
+	    System.out.println("Nome do Pet: "+nomeDoCliente);
+	    System.out.println("Sexo: "+sexoDoPet);
+	    System.out.println("Peso: "+pesoDoPet);
+	    System.out.println("\n---------[ Pagamento ]--------");
+	    System.err.println("Valor Total R$ "+ moedaBr.format(valorTotal));
+	    
+	    
+```
